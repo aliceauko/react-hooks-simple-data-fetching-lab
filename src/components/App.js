eab090b0677d4a20bf173f0692c40e7dd92b50cc
aliceauko <alice.auko@student.moringaschool.com>
2022-07-16 19:38:44 +0300

    commit tree c1c0430b590ac3f5307cf8b5bc348140a7396746

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {useEffect, useState} from "react"
+
+function App (){
+ const [image, setImage] = useState("")
+ const [load, setLoad] = useState(false)
+
+    useEffect(() =>{
+
+        fetch(" https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data =>{
+            setLoad(!load)
+
+            setImage(data.message)
+        })
+    }, [])
+
+    if(!load){
+        return <p>Loading...</p>
+    }
+    return ( <img src ={image} alt="A Random Dog"></img>
+
+    )
+}
+
+export default App
